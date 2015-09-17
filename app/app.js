d7ba8ca5c763ca62b4882bb47d1119af83990f7e
Arun Selvaraj <zeezey@Aruns-MacBook-Pro.local>
2015-09-17 16:26:20 -0600

    s

--- app/app.js
+++ app/app.js
@@ -17,10 +17,10 @@ angular.module('myApp', ['ngRoute'])
     };
 
     function fetch(){
-      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      $http.get("https://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
        .success(function(response){ $scope.details = response; });
 
-      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      $http.get("https://www.omdbapi.com/?s=" + $scope.search)
        .success(function(response){  $scope.related = response; });
     }
 
